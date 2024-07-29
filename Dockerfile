d40cb329e982763020a62f9be853446eea88a886
sladeque <dstitov@gmail.com>
2024-07-30 00:09:47 +0300

    1st commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Run random_password.py when the container launches
+CMD ["python", "random_password.py"]
---
+++ random_password.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+def generate_password(length=12, use_uppercase=True, use_digits=True, use_special=True):
+    characters = string.ascii_lowercase
+    if use_uppercase:
+        characters += string.ascii_uppercase
+    if use_digits:
+        characters += string.digits
+    if use_special:
+        characters += string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password = generate_password()
+    print(password)
